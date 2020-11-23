be49e8ce32b511f5d7bdbb0fee914c62377f1f1e
cfk <cfk-source@users.noreply.github.com>
2020-11-23 20:50:46 +0200

    triangle area calculator v1.0

---
+++ programm1.py
@@ -0,0 +1,8 @@
+a = int(input("Enter triangle side A length "))
+b = int(input("Enter triangle side B length "))
+c = int(input("Enter triangle side C length "))
+
+z = (a + b + c) // 2
+area = (z *(z - a) * (z - b) * (z - c)) ** 0.5
+print("Side A =", a + "Side B =", b + "Side C =", c)
+print("Triangle area is ", area)
