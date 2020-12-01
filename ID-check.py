aaf2e8e0fe2e5d2cc4f80e3a89cac3c36a2556ee
cfk <cfk-source@users.noreply.github.com>
2020-12-01 13:35:57 +0200

    triangle area calculator v1.0

--- ID-check.py
+++ ID-check.py
@@ -2,36 +2,47 @@
 while True:
     try:
         id_input = input('Palun sisestage isikukood: ')
+
+        # char check
         id_int = int(id_input)
+
+        # len check
         if len(id_input) != 11:
             print('Isikukoodil peab olema 11 numbrid, palun proovige uuesti.')
+
+        # Control number check
         else:
-            id_int = int(id_input)
-            break
-    except:
-        print('Palun sisetage ainult nubrid.')
+            check_scale_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1]
+            check_scale_2 = [3, 4, 5, 6, 7, 8, 9, 1, 2, 3]
+            check_sum_1 = 0
+            check_sum_2 = check_sum_1
 
-# last digit control check
+            # scale check
+            for x in range(0, 10):
+                check_sum_1 = check_sum_1 + int(id_input[x]) * check_scale_1[x]
+                check_sum_2 = check_sum_2 + int(id_input[x]) * check_scale_2[x]
 
-check_scale_1 = [1,2,3,4,5,6,7,8,9,1]
+            #
+            control_number_1 = check_sum_1 % 11
+            control_number_2 = check_sum_2 % 11
 
-check_scale_2 = [3,4,5,6,7,8,9,1,2,3]
-
-# x = id_input()                              # ID user input (string!)
-# y = id_int[10]                              # last digit in ID
-# while True (x[0] * 1) + (x[1] * 2) + (x[3] * 3) + (x[4] * 4) + (x[5] * 5) + (x[6] * 6) + (x[7] * 7) + (x[8] * 8) + (x[9] * 9) + (x[10] * 1) / 11 != int(id_int[11]) or
-#                     (x[0] * 3) + (x[1] * 4) + (x[3] * 5) + (x[4] * 6) + (x[5] * 7) + (x[6] * 8) + (x[7] * 9) + (x[8] * 1) + (x[9] * 2) + (x[10] * 3) / 11 != int(id_int[11]):
-
+            #
+            if (10 > control_number_1 == int(id_input[10])) or (
+                    int(id_input[10]) == control_number_2 or
+                    control_number_2 == 10 & int(id_input[10]) == 0):
+                break
+            else:
+                print('Isikukoodi viga, palun proovige uuesti.')
+    except ValueError:
+        print('Isikukoodil peab olema ainult numbrid, palun proovige uuesti.')
 
 sex_id = id_input[0]
-birth_year = id_input[1:3]              # YY
-birth_month = id_input[3:5]             # MM
-birth_day = id_input[5:7]               # DD
+birth_year = id_input[1:3]  # YY
+birth_month = id_input[3:5]  # MM
+birth_day = id_input[5:7]  # DD
 birth_place = int(id_input[7:10])
 
-
-#sex and year extraction
-
+# sex and year check
 if sex_id == '1':
     sex = 'mees'
     by = '18'
@@ -57,7 +68,8 @@ if 0 <= birth_place <= 10:
 elif 11 <= birth_place <= 19:
     print("Te olite sündinud Kuressaare Tartu Ülikooli Naistekliinikus, Tartumaa, Tartu")
 elif 21 <= birth_place <= 220:
-    print("Te olite sündinud Ida-Tallinna Keskhaiglas või Pelgulinna sünnitusmajas või Hiiumaal või Keilas või Rapla haiglas või Loksa haiglas")
+    print("Te olite sündinud Ida-Tallinna Keskhaiglas või Pelgulinna "
+          "sünnitusmajas või Hiiumaal või Keilas või Rapla haiglas või Loksa haiglas")
 elif 221 <= birth_place <= 270:
     print("Te olite sündinud Ida-Viru Keskhaiglas (Kohtla-Järve, endine Jõhvi)")
 elif 271 <= birth_place <= 370:
@@ -77,11 +89,9 @@ elif 571 <= birth_place <= 600:
 elif 601 <= birth_place <= 650:
     print("Te olite sündinud Viljandi Haiglas")
 elif 651 <= birth_place <= 700:
-                print("Te olite sündinud Lõuna-Eesti Haiglas (Võru) või Põlva Haiglas")
+    print("Te olite sündinud Lõuna-Eesti Haiglas (Võru) või Põlva Haiglas")
 
-
-
-#Print results of code analysis
+# Results of last two checks
 
 print('Te olete sündinud: ' + by + birth_year + '.' + birth_month + '.' + birth_day)
 print('Sugu ' + sex)
