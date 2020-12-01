601d20cf5d9d73d60c14a319f10a25f3306f8081
cfk <cfk-source@users.noreply.github.com>
2020-12-01 17:51:46 +0200

    ID_check

--- fID_validation.py
+++ fID_validation.py
@@ -13,7 +13,7 @@ while True:
 
         # length check
         len(id) == 11
-
+        print('Your personal ID-code is: ' + id)
         # birth date extraction NB!!! STRINGS
         bd = id[0:2]    # "DD"
         bm = id[2:4]    # "MM"
@@ -21,19 +21,43 @@ while True:
         c = id[6]       # "C" century sign: ("-" 1900-1999),("+" 1800-1899),("A" 2000-2099)
         z = id[7:10]    # "ZZZ" Individual number
         q = id[10]      # "Q" Control char.checksum
+        b_day = str(bd) + '.' + str(bm) + '.19' + str(by)
+        b_day_formated = datetime.datetime.strptime(b_day, '%d.%m.%Y')              # birth date human_readable
+        today = datetime.date.today()
+        age = today.year - b_day_formated.year - (
+                (today.month, today.day) < (b_day_formated.month, b_day_formated.day))
+
+        # sex
+        if int(z) % 2 == 0:
+            sex = 'female'
+        else:
+            sex = 'man'
+
+        # personal number status - permanent / temporary
+        if int(z) >= 2 & int(z) <= 899:
+            id_state = ' permanent'
+        else:
+            id_state = ' temporary'
+        print('Personal code is: ' + id_state)
 
         # birth date check & output according to 'C'entury information
         if c == '+':
-            print('Your birth date is: ', str(18) + by + '.' + bm + '.' + bd + '')
+            print('Your birth date is: ', str(18) + by + '.' + bm + '.' + bd + ' and you are ' + str(age) + ' full years ' + sex + '.')
 
         elif c == '-':
-            print('Your birth date is: ', str(19) + by + '.' + bm + '.' + bd)
+            print('Your birth date is: ', str(19) + by + '.' + bm + '.' + bd + ' and you are ' + str(age) + ' full years ' + sex + '.')
 
         elif c == 'A':
-            print('Your birth date is: ', str(20) + by + '.' + bm + '.' + bd)
+            print('Your birth date is: ', str(20) + by + '.' + bm + '.' + bd + ' and you are ' + str(age) + ' full years ' + sex + '.')
         else:
-            print('Wrong century information.')
             break
+            print('Wrong century information.')
+
+        # control character check - "DDMMYY.ZZZ"
+        control_char = (int(id[0:10]) - int(id[6])) / 31
+
+
+
 
 
 
