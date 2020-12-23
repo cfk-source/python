05e1120b4b6dd3d620b2ca0114cf4245501449aa
cfk <cross.kay.foster@gmail.com>
2020-12-23 16:06:02 +0200

    web scraper

---
+++ requests/login_session/scrape.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup as BS, BeautifulSoup
+
+soup = BeautifulSoup()
+
+data = {'csrf': 'DMzC8wcInE3fMYuQ0A6QdQ==',
+  'nick': 'cfk',
+  'passwd': '44914495'}
+
+cookies = {'CSRF_TOKEN': 'DMzC8wcInE3fMYuQ0A6QdQ==',
+    '__utma': '75309071.1655505578.1608729783.1608729783.1608729783.1',
+    '__utmc': '75309071',
+    '__utmz': '75309071.1608729783.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)',
+    '__utmt': '1',
+    'JSESSIONID': '640C1E94C242A4913B83CA138594194B',
+    '__utmb': '75309071.4.10.1608729783'}
+
+url = 'https://www.linux.org.ru/login_process'
+s = requests.Session()
+
+response = s.post(url, data=data, cookies=cookies)
+
+notifications = soup.findAll('<span id="main_events_count">(1)</span>'[30])
+
+
+print(notifications)
---
+++ selenium/test.py
@@ -0,0 +1,4 @@
+import selenium
+from selenium import webdriver
+chrome_driver = 'sources/chromedriver'
+driver = webdriver.Chrome(chrome_driver)
